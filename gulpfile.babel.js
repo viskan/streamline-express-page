39478e006aa4102e49ea88fcbfc65566f214499b
gillstrom <andreasgillstrom@gmail.com>
2016-09-14 16:34:15 +0200

    Improve workflow

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,18 +1,26 @@
 'use strict';
 const estorePreprocessor = require('estore-preprocessor');
 const gulp = require('gulp');
+const gulpChanged = require('gulp-changed');
 const gulpPlumber = require('gulp-plumber');
 const gulpPostcss = require('gulp-postcss');
 
 gulp.task('styles', () => {
-	return gulp.src('css/index.css')
+	return gulp.src('source/css/index.css')
 		.pipe(gulpPlumber())
 		.pipe(gulpPostcss([estorePreprocessor({path: 'css'})]))
 		.pipe(gulp.dest('build'));
 });
 
-gulp.task('watch', () => {
-	gulp.watch(['css/**/*.css'], ['styles']);
+gulp.task('copy', () => {
+	return gulp.src(['source/**/*.{default,eot,gif,html,jpg,jsp,png,svg,ttf,woff,xml}'])
+		.pipe(gulpChanged('build'))
+		.pipe(gulp.dest('build'));
 });
 
-gulp.task('default', ['styles']);
+gulp.task('watch', ['default'], () => {
+	gulp.watch(['source/css/**/*.css'], ['styles']);
+	gulp.watch(['source/**/*.{default,eot,gif,html,jpg,jsp,png,svg,ttf,woff,xml}'], ['copy']);
+});
+
+gulp.task('default', ['copy', 'styles']);
