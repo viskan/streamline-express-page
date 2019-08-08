3c07530777b225f41c9b679aecb5fc444a0aeedd
Sebastian Midborn <sebastian.midborn@viskan.se>
2019-08-08 13:35:09 +0200

    Replace deprecated Gulp with Parcel

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	plugins: {
+		'postcss-import': {},
+		'postcss-apply': {},
+		'postcss-for': {},
+		'postcss-nested': {},
+		'postcss-fontpath': {},
+		'postcss-custom-properties': {},
+		'cssnano': {autoprefixer: false},
+		'postcss-preset-env': {}
+	}
+};
